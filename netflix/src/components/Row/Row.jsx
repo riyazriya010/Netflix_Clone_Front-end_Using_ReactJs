b5d7394cfcbfb65e4c91e44449725088beea6941
riyazriya010 <riyur017@gmail.com>
2024-09-17 12:09:12 +0530

    app created and hosted on firebase

--- netflix/src/components/Row/Row.jsx
+++ netflix/src/components/Row/Row.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from '../../services/axios'
 import './row.css';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer'
 
 const base_url = `https://image.tmdb.org/t/p/original/`;
 
 function Row( { title, fetchUrl ,isLargeRow }  ) {
     const [movies, setMovies] = useState( [] );
+    const [trailerUrl, setTrailerUrl] = useState("");
 
     useEffect(() =>{
 
@@ -21,7 +24,34 @@ function Row( { title, fetchUrl ,isLargeRow }  ) {
         // everytime outside variable using inside the useEffect we want to add in the array to track the changes
     }, [fetchUrl]);
 
-    console.log(movies)
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            autoplay:1,
+        }
+    }
+
+    const handleClick = (movie) => {
+        console.log(movie)
+        if (trailerUrl) {
+            setTrailerUrl("");
+        } else {
+            movieTrailer(movie?.name || movie?.title || movie?.original_name || movie?.original_title || "")
+                .then((url) => {
+                    if (url) {
+                        const urlParams = new URLSearchParams(new URL(url).search);
+                        setTrailerUrl(urlParams.get("v"));
+                    } else {
+                        console.log("No trailer found");
+                    }
+                })
+                .catch((error) => console.error("Error fetching trailer: ", error));
+        }
+    };
+
+
+    // console.log(movies)
 
 
     return(
@@ -36,6 +66,7 @@ function Row( { title, fetchUrl ,isLargeRow }  ) {
             {
                 movies.map(movie => {
                     return <img 
+                    onClick={() => handleClick(movie)}
                     className={`row_poster_img ${isLargeRow && `row_posterLarge_img`}`} 
                     src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
                     alt={movie.name} 
@@ -43,9 +74,9 @@ function Row( { title, fetchUrl ,isLargeRow }  ) {
                 })
             }
         </div>
-
+        { trailerUrl && <YouTube videoId={trailerUrl} otps={opts}/> }
     </div>
-    )
+    );
 }
 
 export default Row;
